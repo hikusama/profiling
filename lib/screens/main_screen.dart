03b5d991b1d7ded0369cf6e313b4113f62bdc520
Hikusama <leomessi25k@gmail.com>
2025-04-10 07:33:40 +0800

    mvn

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:profiling/screens/contents/migrate.dart';
+import 'package:profiling/screens/contents/overview.dart';
+import 'package:profiling/screens/contents/record.dart';
+import 'package:profiling/screens/contents/settings.dart';
 
 
 class MainScreen extends StatefulWidget {
@@ -8,4 +12,21 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
+class _MainScreenState extends State<MainScreen> {
+
+  int _selectedIndex = 0;
+
+
+  final List<Widget> _screens = [
+    const OverviewScreen(),
+    const RecordScreen(),
+    const MigrateScreen(),
+    const SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
 
