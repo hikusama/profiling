cbb6805368049e4f3390b00e0205c475b6e68450
Hikusama <leomessi25k@gmail.com>
2025-04-09 10:27:12 +0800

    nyow

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+
