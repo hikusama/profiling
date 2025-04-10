203766ce2e6d53b2070082d669cf4807513e1373
Hikusama <leomessi25k@gmail.com>
2025-04-10 08:35:44 +0800

    UI navs

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -4,7 +4,6 @@ import 'package:profiling/screens/contents/overview.dart';
 import 'package:profiling/screens/contents/record.dart';
 import 'package:profiling/screens/contents/settings.dart';
 
-
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
@@ -13,9 +12,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-
   int _selectedIndex = 0;
-
 
   final List<Widget> _screens = [
     const OverviewScreen(),
@@ -24,9 +21,55 @@ class _MainScreenState extends State<MainScreen> {
     const SettingsScreen(),
   ];
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 8.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildNavItem(Icons.grid_view, "Overview", 0),
+            _buildNavItem(Icons.grid_view, "Records", 1),
+            const SizedBox(width: 40),
+            _buildNavItem(Icons.grid_view, "Migrate", 2),
+            _buildNavItem(Icons.grid_view, "Settings", 3),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        shape: const CircleBorder(),
+        onPressed: () => {},
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, String label, int index) {
+    final bool isSelected = _selectedIndex == index;
+    return InkWell(
+      onTap: () => _onItemTapped(index),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(icon, color: isSelected ? Colors.black : Colors.grey),
+          Text(
+            label,
+            style: TextStyle(color: isSelected ? Colors.black : Colors.grey),
+          ),
+        ],
+      ),
+    );
   }
 }
-
