fdf71ab73f9baa3818d42e5e84175b0c90ddcd24
Hikusama <leomessi25k@gmail.com>
2025-04-12 08:07:23 +0800

    Inserting ui stepper format%

--- lib/actions/insert.dart
+++ lib/actions/insert.dart
@@ -8,24 +8,49 @@ class InsertYouth extends StatefulWidget {
 }
 
 class _InsertYouthState extends State<InsertYouth> {
-
   int currStep = 0;
   bool get isFirstStep => currStep == 0;
-  // bool get isLastStep => currStep == steps().lenght - 1;
+  bool get isLastStep => currStep == steps().length - 1;
 
   @override
-  Widget build(BuildContext context) {
-    List<Step> steps() => [
-      const Step(
-        title: Text('Personal Information'),
-        content: Text('Personal Information'),
+  Widget build(BuildContext context) => Scaffold(
+    body: Stepper(
+      steps: steps(),
+      currentStep: currStep,
+      onStepContinue: () {
+        if (isLastStep) {
+        } else {
+          setState(() {
+            currStep += 1;
+          });
+        }
+      },
+      onStepCancel: isFirstStep ? null : () => setState(() => currStep -= 1),
+      onStepTapped: (step) => setState(() => currStep = step),
+    ),
+  );
+
+  List<Step> steps() => [
+    Step(
+      isActive: currStep >= 0,
+      title: Text('Personal Information'),
+      content: Column(
+        
       ),
-      const Step(
-        title: Text('Address & Birth Details'),
-        content: Text('Address & Birth Details'),
+    ),
+    Step(
+      isActive: currStep >= 1,
+      title: Text('Address & Birth Details'),
+      content: Column(
+        
       ),
-      const Step(title: Text('Other Details'), content: Text('Other Details')),
-    ];
-    return const Placeholder();
-  }
+    ),
+    Step(
+      isActive: currStep >= 2,
+      title: Text('Other Details'),
+      content: Column(
+        
+      ),
+    ),
+  ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:profiling/screens/main_screen.dart';
+import 'package:profiling/actions/insert.dart';
+// import 'package:profiling/screens/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MainScreen(),
+      debugShowCheckedModeBanner: false,
+      home: InsertYouth(),
     );
   }
 }
